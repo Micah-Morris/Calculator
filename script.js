ab98ef8a9f886ff181bf81b0ee816fbf9a52aed1
Micah Morris <micah.morris42.com>
2023-03-03 10:42:32 -0700

    Create functions for -, *, and /

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ function add(a, b) {
     let sum = a + b;
     console.log(sum);
 }
+function subtract(a, b) {
+    let sum = a -b;
+    console.log(sum);
+}
+function multiply(a, b) {
+    let sum = a * b;
+    console.log(sum);
+}
+function divide(a, b) {
+    let sum = a / b;
+    console.log(sum);
+}
