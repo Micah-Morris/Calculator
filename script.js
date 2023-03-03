d06a7da98f3810ed976497381128417111975650
Micah Morris <micah.morris42.com>
2023-03-03 10:50:22 -0700

    create operate func to check operator and call proper func

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ function divide(a, b) {
     let sum = a / b;
     console.log(sum);
 }
+function operate(operator, a, b) {
+    if (operator === '+') {
+        add(a, b)
+    }
+    else if (operator === '-'){
+        subtract(a, b)
+    }
+    else if (operator === '*'){
+        multiply(a, b)
+    }
+    else if (operator === '/'){
+        divide(a, b)
+    }
+}
+
