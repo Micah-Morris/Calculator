fe7a4c1e1426dda6a6058ab8db0b3f3ff59b1499
Micah Morris <micah.morris42.com>
2023-03-03 12:56:37 -0700

    create consts for all buttons

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-const numOne = document.querySelector()
+const numOne = document.getElementById('1')
+const numTwo = document.getElementById('1')
+const numThree = document.getElementById('1')
+const numFour = document.getElementById('1')
+const numFive = document.getElementById('1')
+const numSix = document.getElementById('1')
+const numSeven = document.getElementById('1')
+const numEight = document.getElementById('1')
+const numNine = document.getElementById('1')
+const numZero = document.getElementById('1')
+const plusSign = document.getElementById('1')
+const minusSign = document.getElementById('1')
+const multiplySign = document.getElementById('1')
+const divideSign = document.getElementById('1')
+const equalSign = document.getElementById('1')
+const decimalPoint = document.getElementById('1')
+const trash = document.getElementById('1')
 
+console.log(numOne);
 function add(a, b) {
     let sum = a + b;
     console.log(sum);
